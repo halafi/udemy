13bcbed86fee38dfe1da6204ab9e87d3237682dc
filip.halas <filip.halas@kiwi.com>
2017-08-12 16:09:17 +0200

    authentication form finish

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,21 +1,39 @@
 import React, { Component } from 'react';
-import { View, TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 export default class LoginForm extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <Card>
         <CardSection>
-          <TextInput style={{ height: 20, width: 100, color: "black" }}/>
+          <Input
+              placeholder="user@gmail.com"
+              label="Email"
+              value={email}
+              onChangeText={email => this.setState({ email })}
+          />
         </CardSection>
 
-        <CardSection/>
+        <CardSection>
+          <Input
+              placeholder="password"
+              label="Password"
+              value={password}
+              onChangeText={password => this.setState({ password })}
+              secureTextEntry
+          >
+          </Input>
+        </CardSection>
 
         <CardSection>
-          <Button>
-            Log In
-          </Button>
+          <Button>Log In</Button>
         </CardSection>
       </Card>
     )
