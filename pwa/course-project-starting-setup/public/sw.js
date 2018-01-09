99743cd0ceedb5096421e625e5f291157d8d8c94
filip.halas <filip.halas@kiwi.com>
2018-01-09 18:27:45 +0100

    feat(pwa): static caching

--- pwa/course-project-starting-setup/public/sw.js
+++ pwa/course-project-starting-setup/public/sw.js
@@ -1,13 +1,42 @@
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener("install", function(event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open("static").then(function(cache) {
+      console.log("[Service Worker] Precaching App Shell");
+      // pre-caching (static)
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/material.min.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/images/main-image.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700", // loads external resource
+        "https://fonts.googleapis.com/icon?family=Material+Icons", // loads external resource
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css"
+      ]);
+      // dynamic caching
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ...', event);
+self.addEventListener("activate", function(event) {
+  console.log("[Service Worker] Activating Service Worker ...", event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ...', event);
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", function(event) {
+  console.log("[Service Worker] Fetching something ...", event);
+  // event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      // null response if not found
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
 });
