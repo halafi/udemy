c48f0769f3a83ddadcdf2e182de7bcf4e5efbc0b
filip.halas <filip.halas@kiwi.com>
2018-06-13 10:57:13 +0200

    feat(node): nodestarter

--- nodejs/1-cluster/index.js
+++ nodejs/1-cluster/index.js
@@ -1,37 +1,30 @@
-process.env.UV_THREADPOOL_SIZE = 1;
-
 const express = require('express');
-const cluster = require('cluster');
-const crypto = require('crypto');
-const numCPUs = require('os').cpus().length;
+
 const app = express();
+const Worker = require("webworker-threads").Worker;
 
-// console.log(cluster.isMaster, 'isMaster');
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running`);
+app.get("/", (req, res) => {
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+  const worker = new Worker(function() {
+    this.onmessage = function() {
+      let counter = 0;
+      while (counter < 1e9) {
+        counter++;
+      }
+      postMessage(counter);
+    }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died`);
   });
-} else {
-  app.get("/", (req, res) => {
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-      res.send("Hi there");
-    });
-    console.log(`Worker ${process.pid} assigned to slow`);
-    res.send("Hi there");
-  });
-  app.get("/fast", (req, res) => {
-    console.log(`Worker ${process.pid} assigned to fast`);
-    res.send("Fast");
-  });
-  app.listen(3000);
 
-  console.log(`Worker ${process.pid} started`);
-}
+  worker.onmessage = function(myCounter) {
+    console.log(myCounter, 'result')
+  };
 
+  worker.postMessage();
+});
+
+app.get("/fast", (req, res) => {
+  res.send("Fast");
+});
+
+app.listen(3000);
---
+++ nodejs/AdvancedNodeStarter/config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://filip:password12@ds159020.mlab.com:59020/blog_dev',
+  cookieKey: '123123123'
+};
