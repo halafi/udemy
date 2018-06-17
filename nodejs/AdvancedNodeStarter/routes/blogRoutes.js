4eb26fbffba778daeacd9f5410be3e625f3856b0
filip.halas <filip.halas@kiwi.com>
2018-06-17 16:45:41 +0200

    feat(node): caching wip

--- nodejs/AdvancedNodeStarter/routes/blogRoutes.js
+++ nodejs/AdvancedNodeStarter/routes/blogRoutes.js
@@ -14,9 +14,25 @@ module.exports = app => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const util = require('util');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+    client.get = util.promisify(client.get);
+
+    const cachedBlogs = await client.get(req.user.id);
+
+    if (cachedBlogs) {
+      console.log('serving from cache');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
     const blogs = await Blog.find({ _user: req.user.id });
 
+    console.log('serving from db');
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
