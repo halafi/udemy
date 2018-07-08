2f185e737b03be9f5363d6ecef909ad041b3e00c
halafi <filiphalas74@gmail.com>
2018-07-08 10:05:11 +0200

    feat(nodejs): caching finish

--- nodejs/AdvancedNodeStarter/routes/blogRoutes.js
+++ nodejs/AdvancedNodeStarter/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
+const cleanCache = require('../middlewares/cleanCache');
 
 const Blog = mongoose.model('Blog');
 
@@ -13,26 +14,12 @@ module.exports = app => {
     res.send(blog);
   });
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const redis = require('redis');
-    const util = require('util');
-    const redisUrl = 'redis://127.0.0.1:6379';
-    const client = redis.createClient(redisUrl);
-    client.get = util.promisify(client.get);
+  app.get('/api/blogs', requireLogin, cleanCache, async (req, res) => {
+    const blogs = await Blog.find({ _user: req.user.id }).cache({
+      key: req.user.id,
+    });
 
-    const cachedBlogs = await client.get(req.user.id);
-
-    if (cachedBlogs) {
-      console.log('serving from cache');
-      return res.send(JSON.parse(cachedBlogs));
-    }
-
-    const blogs = await Blog.find({ _user: req.user.id });
-
-    console.log('serving from db');
     res.send(blogs);
-
-    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
