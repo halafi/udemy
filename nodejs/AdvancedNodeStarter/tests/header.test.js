a342c2f0eaef813b8a9b78ba481809648afc8a5f
halafi <filiphalas74@gmail.com>
2018-07-15 17:07:28 +0200

    feat(node): browser tests

---
+++ nodejs/AdvancedNodeStarter/tests/header.test.js
@@ -0,0 +1,30 @@
+const puppeteer = require("puppeteer");
+
+let browser, page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false
+  });
+
+  page = await browser.newPage();
+
+  await page.goto("localhost:3000");
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test("we can launch a browser", async () => {
+  const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+
+  expect(text).toEqual("Blogster");
+});
+
+test("clicking login starts oauth flow", async () => {
+  await page.click(".right a");
+  const url = await page.url();
+
+  expect(url).toMatch(/accounts\.google\.com/);
+});
